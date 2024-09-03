4e1f1da43a129e76c58d0cb698658a45340983c1
Andres Caceres <8844096+pinkfloydsito@users.noreply.github.com>
2024-09-03 20:11:47 +0300

    AnkiGenerator class created

--- lib/anki_generator.rb
+++ lib/anki_generator.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+require 'yaml'
+require 'anki'
+
+# AnkiGenerator -> Class
 class AnkiGenerator
+  attr_accessor :deck_file, :cards
+
   def initialize(deck_file:)
     self.deck_file = deck_file
     self.cards = []
@@ -9,6 +15,19 @@ class AnkiGenerator
   end
 
   def fill_cards
-    puts 'test'
+    self.cards = YAML.load_file(deck_file)
+  end
+
+  def generate_apkg(output_file:)
+    deck = Anki::Deck.new('Generated Anki Deck')
+
+    cards.each do |card|
+      deck.add_note(
+        card['front'],
+        card['back']
+      )
+    end
+
+    deck.save(output_file)
   end
 end
